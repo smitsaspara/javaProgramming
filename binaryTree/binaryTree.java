64aeaa3ede0f9daeeed465a66653131fc4a9d5e3
Smit Saspara <smit@Smits-MBP.lan>
2024-06-21 04:49:58 -0500

    implemented inorder and postorder binary Tree

--- binaryTree/binaryTree.java
+++ binaryTree/binaryTree.java
@@ -58,7 +58,7 @@ public class binaryTree {
 
             TreeNode temp = st.pop();
 
-            System.out.println(temp.data + " ");
+            System.out.print(temp.data + " ");
 
             if(temp.right != null){
                 st.push(temp.right);
@@ -70,9 +70,101 @@ public class binaryTree {
         }
     }
 
+    //recursive
+    public void inOrderR(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        inOrderR(root.left);
+        System.out.print(root.data+ " ");
+        inOrderR(root.right);
+    }
+
+    //Iterative
+    public void inOrderI(TreeNode root){
+        if (root == null) {
+            return;            
+        }
+
+        Stack<TreeNode> st = new Stack<>();
+
+        TreeNode temp = root;
+
+        while (!st.isEmpty() || temp != null){
+            if (temp != null) {
+                st.push(temp);
+                temp = temp.left;
+            }
+            else{
+                temp = st.pop();
+                System.out.print(temp.data + " ");
+                temp = temp.right;
+            }
+        }
+    }
+
+    //Recursive
+    public void postOrderR(TreeNode root){
+        if (root == null) {
+            return;            
+        }
+        postOrderR(root.left);
+        postOrderR(root.right);
+        System.out.print(root.data + " ");
+    } 
     
+    //Iterative
+    public void postOrderI(TreeNode root){
+        TreeNode current = root;
+
+        Stack<TreeNode> st = new Stack<>();
+
+        while (!st.isEmpty() || current != null ) {
+
+            if (current != null) {
+
+                st.push(current);
+                current = current.left;
+            }
+            else{
+
+                TreeNode temp = st.peek().right;
+                if(temp == null){
+                   
+                    temp = st.pop();
+
+                    System.out.print(temp.data + " ");
+
+                    while (!st.isEmpty() && temp == st.peek().right) {
+                        temp = st.pop();
+
+                        System.out.print(temp.data + " ");
+                    }
+                }
+                else{
+                    current = temp;
+                }
+
+            }
+            
+        }
+    } 
+
     public static void main(String[] args) {
         binaryTree bt = new binaryTree();
         bt.createBinaryTree();
+        bt.preOrderR(bt.root);
+        System.out.println();
+        bt.preOrderI(bt.root);
+        System.out.println();
+        bt.inOrderR(bt.root);
+        System.out.println();
+        bt.inOrderI(bt.root);
+        System.out.println();
+        bt.postOrderR(bt.root);
+        System.out.println();
+        bt.postOrderI(bt.root);
+
     }
 }
