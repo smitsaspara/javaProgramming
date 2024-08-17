222061049260e7f17db30d73c6c1fd7a7d791868
Smit Saspara <smit@Smits-MacBook-Pro.local>
2024-08-17 00:41:11 -0500

    binarySearchTree

--- binaryTree/binarySearchTree.java
+++ binaryTree/binarySearchTree.java
@@ -14,5 +14,51 @@ public class binarySearchTree {
         }
     }
 
+    public void insert(int value){
+        root = insert(root, value);
+    }
+
+    //recursive insert
+    public TreeNode insert(TreeNode root, int value){
+        if(root == null ){
+            root = new TreeNode(value);
+            return root; 
+        }
+
+        if(value < root.data){
+            root.left = insert(root.left, value);
+        }else{
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    //inorder
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    public void inOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data);
+        inOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        binarySearchTree bst = new binarySearchTree();
+        bst.insert(6);
+        bst.insert(2);
+        bst.insert(8);
+        bst.insert(0);
+        bst.insert(10);
+
+        bst.inOrder();
+
+    }
+
     
 }
