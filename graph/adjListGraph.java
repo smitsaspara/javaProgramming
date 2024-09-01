f2763c90c2f363c3e204192473c89ea329d2e0ed
Smit Saspara <smit@Smits-MacBook-Pro.local>
2024-09-01 03:32:45 -0500

    graph using LinkedList & dfs and bfs

--- graph/adjListGraph.java
+++ graph/adjListGraph.java
@@ -1,6 +1,10 @@
 package graph;
 
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import stack.stack;
 
 public class adjListGraph {
 
@@ -38,13 +42,62 @@ public class adjListGraph {
         return sb.toString();
     }
 
+    //bfs traversal
+    public void bfs(int s){
+        boolean[] isVisited = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+        isVisited[s] = true;
+        q.offer(s);
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            System.out.print(u +" ");
+            for (int v : adj[u]) {
+                if (!isVisited[v]) {
+                    isVisited[v] = true;
+                    q.offer(v);
+                }
+            }
+
+        }
+    }
+
+    //dfs traversal
+    public void dfs(int s){
+        boolean[] isVisited = new boolean[V];
+        Stack<Integer> st = new Stack<>();
+        st.push(s);
+        while (!st.isEmpty()) {
+            int u = st.pop();
+            if (!isVisited[u]) {
+                isVisited[u] = true;
+                System.out.print(u + " ");
+
+                for (int v : adj[u]) {
+                    if (!isVisited[v]) {
+                        st.push(v);
+                    }
+                }
+
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        adjListGraph g = new adjListGraph(4);
+        adjListGraph g = new adjListGraph(5);
         g.addEdge(0, 1);
         g.addEdge(1, 2);
         g.addEdge(2, 3);
         g.addEdge(3, 0);
+        g.addEdge(2, 4);
         System.out.println(g);
+        g.bfs(0);
+
+        System.out.println(g);
+
+        g.dfs(0);
+
+
+
     }
     
 }
