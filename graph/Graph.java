170d57c7a19503c2ff8a5be08e8f93aeaaf026fb
Smit Saspara <smit@Smits-MacBook-Pro.local>
2024-09-01 01:18:07 -0500

    graph

---
+++ graph/Graph.java
@@ -0,0 +1,46 @@
+package graph;
+
+public class Graph {
+    private int V;
+    private int E;
+
+    private int[][] adjMatrix;
+
+    public Graph(int n){
+        this.V = n;
+        this.E = 0;
+        this.adjMatrix = new int[n][n];
+    }
+
+    public void addEdge(int u, int v){
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;
+        E++;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(V + " vertices, " + E + " edges " + "\n");
+        for (int v = 0; v < V; v++) {
+            sb.append(v + ": ");
+            for (int w : adjMatrix[v]) {
+                sb.append(w + " ");
+            }
+            
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 01);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+        System.out.println(g);
+    }
+
+
+}
