456a91d20df4b27ceac69e7cce158fe8b8d72b99
Smit Saspara <smit@Smits-MacBook-Pro.local>
2024-09-03 02:04:50 -0500

    hashTable implementation

---
+++ hashing/hashTable.java
@@ -0,0 +1,71 @@
+package hashing;
+
+public class hashTable{
+
+    private HashNode[] buckets;
+    private int capacity; 
+    private int size; //number of pairs
+
+    public hashTable(){
+        this(10);
+    }
+
+    public hashTable(int capacity){
+        this.capacity = capacity;
+        this.buckets = new HashNode[capacity];
+        this.size = 0;
+    }
+
+    private class HashNode{
+        private Integer key;
+        private String value;
+        private HashNode nextHashNode;
+
+        public HashNode(Integer key, String value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void put(Integer key, String value){
+        if (key == null || value == null) {
+            throw new IllegalArgumentException("key or value not present");
+        }
+
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+        }
+
+        size++;
+        head = buckets[bucketIndex];
+        HashNode node = new HashNode(key, value);
+        node.nextHashNode = head;
+        buckets[bucketIndex] = node;
+    }
+
+    private int getBucketIndex(int key){
+            return key % capacity;
+    }
+    public static void main(String[] args) {
+
+        hashTable ht = new hashTable(10);
+        ht.put(21, "Smit");
+        ht.put(105, "saspara");
+        ht.put(21, "kumar");
+        System.out.println(ht.size);
+    }
+}
